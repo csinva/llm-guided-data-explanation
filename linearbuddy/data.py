e160d11a5f585b627fc58c4f7ed4fbf72586bc78
Chandan Singh <chandan_singh@berkeley.edu>
2023-10-24 08:47:00 -0700

    filter out top vals in dataset

--- linearbuddy/data.py
+++ linearbuddy/data.py
@@ -5,6 +5,52 @@ from sklearn.feature_extraction.text import CountVectorizer
 import pandas as pd
 from os.path import join
 from linearbuddy.config import PATH_TO_REPO
+import sklearn.preprocessing
+import imodels
+
+
+def get_data(dataset_name: str = 'iai', test_size=0.6, random_state=42, filter_top_frac: float = None):
+    '''
+    filter_top_frac: if not None, filter out the top frac of y in the training set
+    '''
+    # Set up data
+    # DSETS_CLASSIFICATION = ['pima_diabetes']
+    # X, y, feature_names = imodels.get_clean_dataset("pima_diabetes")
+
+    # iai
+    if dataset_name == 'iai':
+        X, y, feats_raw, feats_abbrev = get_iai_data()
+        feature_names = feats_abbrev.map(
+            ABBREV_TO_CLEAN_IAI.get).values.tolist()
+    elif dataset_name == 'california_housing':
+        X, y, feature_names = imodels.get_clean_dataset("california_housing")
+
+    # split data
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, random_state=random_state, test_size=test_size,
+    )
+
+    # preprocess split data
+    trans = sklearn.preprocessing.StandardScaler()
+    X_train = trans.fit_transform(X_train)
+    X_test = trans.transform(X_test)
+    X = trans.transform(X)
+
+    transy = sklearn.preprocessing.StandardScaler()
+    y_train = transy.fit_transform(y_train.reshape(-1, 1)).flatten()
+    y_test = transy.transform(y_test.reshape(-1, 1)).flatten()
+    y = transy.transform(y.reshape(-1, 1)).flatten()
+
+    # filter top frac of y
+    if filter_top_frac:
+        idxs_excluding_top_frac = np.argsort(
+            y_train)[:-int(filter_top_frac * len(y_train))]
+        X_train = X_train[idxs_excluding_top_frac]
+        y_train = y_train[idxs_excluding_top_frac]
+
+    print("shapes", X.shape, y.shape, "nunique",
+          np.unique(y).size, '-> train', X_train.shape)
+    return X, y, X_train, X_test, y_train, y_test, feature_names
 
 
 def raw_to_abbrev(feat_name: str):
